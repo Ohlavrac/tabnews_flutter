22a7f3c113306cdbe457112a1c33545dcf778ec0
ohlavrac <joaovicrcarvalho@gmail.com>
2022-11-29 00:03:18 -0300

    add: get contents details with post infos

--- lib/data/datasources/contents_remote_datasource.dart
+++ lib/data/datasources/contents_remote_datasource.dart
@@ -1,5 +1,7 @@
+import '../dto/content_detail_dto.dart';
 import '../dto/content_dto.dart';
 
 abstract class ContentsRemoteDataSource {
   Future<List<ContentDto>?> getContents();
+  Future<ContentDetailDto?> getContentDetail(String user, String slug);
 }
